bd73eb7672e4bccacd6405c6c72ba026d65f3c44
zzz <2543794071@qq.com>
2022-07-01 09:47:26 +0800

    第一次提交

---
+++ src/App.vue
@@ -0,0 +1,33 @@
+<!--  -->
+<template>
+    <div @click="onhandele()">
+        {{ obj.msg }}
+        {{ name }}
+    </div>
+</template>
+
+<script>
+import { reactive, toRef, toRefs } from 'vue';
+export default {
+    name: 'App',
+
+    setup() {
+
+        const obj = reactive({
+            msg: 'nihao vue3',
+            name: 'zhangsan',
+            age: 18
+        })
+        const name = toRef(obj, 'name')
+
+        const onhandele = () => {
+            obj.msg = 'vue'
+            name.value = 'lisi'
+
+        }
+
+        return { onhandele, obj, name }
+    }
+
+}
+</script>
